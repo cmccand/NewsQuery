51e764954e2ee90f77fa35337c3b594390b20933
Chris McCandlish <chris.mccandlish@getbellhops.com>
2019-01-29 23:42:09 -0500

    Updates

---
+++ precache-manifest.21ceb713a7cd102540b85fe19d83c458.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc574682d219f9c3fa80",
+    "url": "/cmccand/NewsQuery/static/js/runtime~main.fc574682.js"
+  },
+  {
+    "revision": "dbf61b535f39120f559d",
+    "url": "/cmccand/NewsQuery/static/js/main.dbf61b53.chunk.js"
+  },
+  {
+    "revision": "22c2880f80ad57e7c373",
+    "url": "/cmccand/NewsQuery/static/js/1.22c2880f.chunk.js"
+  },
+  {
+    "revision": "dbf61b535f39120f559d",
+    "url": "/cmccand/NewsQuery/static/css/main.5f614f84.chunk.css"
+  },
+  {
+    "revision": "56d876cb611e540cabad64fbfe5752c8",
+    "url": "/cmccand/NewsQuery/index.html"
+  }
+];
